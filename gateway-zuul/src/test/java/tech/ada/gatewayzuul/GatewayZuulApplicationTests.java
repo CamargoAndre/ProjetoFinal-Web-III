5864aa1340b1694cace887b315f23c1cf759e071
Andre de Camargo <camargo_and@hotmail.com>
2023-04-27 08:55:18 -0300

    Service Registry e gatway spring cloud funcionando

--- gateway-zuul/src/test/java/tech/ada/gatewayzuul/GatewayZuulApplicationTests.java
+++ gateway-zuul/src/test/java/tech/ada/gatewayzuul/GatewayZuulApplicationTests.java
@@ -1,13 +1,1 @@
-package tech.ada.gatewayzuul;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GatewayZuulApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
--- spring-webflux-2/src/main/java/tech/ada/springwebflux/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package tech.ada.springwebflux.exception;
-
-public class UserNotFoundException extends RuntimeException{
-
-    public UserNotFoundException(String s){
-        super(s);
-    }
-}
